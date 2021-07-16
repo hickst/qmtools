13c6276b1c4be397a2fec16d24e183f78f482842
Tom Hicks <hickst@tohono.com>
2021-07-16 15:59:48 -0700

    Renamed test directory to "test"

---
+++ test/mpltest.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+from matplotlib import cm
+from matplotlib import pyplot as plt
+
+TURNIP8_COLORMAP = cm.get_cmap('PiYG', 8)
+TURNIP8_COLORMAP_R = cm.get_cmap('PiYG_r', 8)
+
+def make_legend_on_axis (ax, cmap):
+  """
+  Draws a legend on the given axis using the given colormap.
+  """
+  box_rng = np.arange(8)
+  ticklabels = ['-4.0', '-3.0', '-2.0', '-1.0', '1.0', '2.0', '3.0', '4.0']
+  label_title = 'Green Values are Better'
+  im = ax.imshow([box_rng], cmap)
+  ax.set_xticks(box_rng)
+  ax.set_xticklabels(ticklabels)
+  ax.set_yticks([])
+  ax.set_yticklabels([])
+  ax.set_title(label_title)
+
+fig, axes = plt.subplots(2, 1)
+pos_ax, neg_ax = axes
+make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
+make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
+plt.show()
