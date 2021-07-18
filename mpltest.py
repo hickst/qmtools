0b252f409f3960b6149e4c6301efd000fd6fe559
Tom Hicks <hickst@tohono.com>
2021-07-17 18:31:20 -0700

    Minimize code by calling methods in qmview module.

--- mpltest.py
+++ mpltest.py
@@ -1,57 +1,17 @@
-import io
-import numpy as np
-import pandas as pd
-
 from matplotlib import cm
 from matplotlib import pyplot as plt
 
-# from qmview import qmview as qmv
-import qmview.qmview as qmview
+import qmview.qmview as qmv
+from qmview.qmview import TURNIP8_COLORMAP, TURNIP8_COLORMAP_R
 
-TURNIP8_COLORMAP = cm.get_cmap('PiYG', 8)
-TURNIP8_COLORMAP_R = cm.get_cmap('PiYG_r', 8)
-
-
-def make_legend_on_axis (ax, cmap):
-  """
-  Draws a legend on the given axis using the given colormap.
-  """
-  box_rng = np.arange(8)
-  ticklabels = ['-4.0', '-3.0', '-2.0', '-1.0', '1.0', '2.0', '3.0', '4.0']
-  label_title = 'Green Values are Better'
-  im = ax.imshow([box_rng], cmap)
-  ax.set_xticks(box_rng)
-  ax.set_xticklabels(ticklabels)
-  ax.set_yticks([])
-  ax.set_yticklabels([])
-  ax.set_title(label_title)
-
-
-def load_tsv (tsv_path):
-  return pd.read_csv(tsv_path, sep='\t')
-
-
-def write_table_to_html (styler, filepath):
-  """
-  Render the given pandas.io.formats.style.Styler object as
-  HTML and write the HTML to the given filepath.
-  """
-  with open(filepath, "w") as outfyl:
-    outfyl.writelines(styler.render())
-
-
-qm_df = load_tsv('test/resources/gtest.tsv')
-norm_df = qmview.normalize_to_zscores(qm_df)
-# the following returns a pandas.io.formats.style.Styler object
-styler = norm_df.style.background_gradient(cmap=TURNIP8_COLORMAP, axis=None, vmin=-4.0, vmax=4.0)
-write_table_to_html(styler, "/tmp/table.html")
-
-# plt.show()
-
-# qmview.colorize_by_std_deviations(norm_df)
-# print(norm_df)
+# qm_df = qmv.load_tsv('test/resources/group_bold.tsv')
+qm_df = qmv.load_tsv('test/resources/gtest.tsv')
+norm_df = qmv.normalize_to_zscores(qm_df)
+styler = qmv.colorize_by_std_deviations(norm_df)
+qmv.write_table_to_html(styler, "/tmp/table.html")
 
 # fig, axes = plt.subplots(2, 1)
 # pos_ax, neg_ax = axes
-# make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
-# make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
+# qmv.make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
+# qmv.make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
+# plt.show()
