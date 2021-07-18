56e17ffb39887dfe80ee0da91ea347ed9713605f
Tom Hicks <hickst@tohono.com>
2021-07-17 21:32:43 -0700

    Add main method and move all logic into it.

--- test/test_traffic_light.py
+++ test/test_traffic_light.py
@@ -1,17 +1,35 @@
+import sys
+
 from matplotlib import cm
 from matplotlib import pyplot as plt
 
 import qmview.traffic_light as traf
 from qmview.traffic_light import TURNIP8_COLORMAP, TURNIP8_COLORMAP_R
 
-# qm_df = traf.load_tsv('test/resources/group_bold.tsv')
-qm_df = traf.load_tsv('test/resources/gtest.tsv')
-norm_df = traf.normalize_to_zscores(qm_df)
-styler = traf.colorize_by_std_deviations(norm_df)
-traf.write_table_to_html(styler, "/tmp/table.html")
+def main (argv=None):
+  """
+  The main method for the tool. This method is called from the command line,
+  processes the command line arguments and calls into the ImdTk library to do its work.
+  This main method takes no arguments so it can be called by setuptools.
+  """
 
-# fig, axes = plt.subplots(2, 1)
-# pos_ax, neg_ax = axes
-# traf.make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
-# traf.make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
-# plt.show()
+  # the main method takes no arguments so it can be called by setuptools
+  if (argv is None):                      # if called by setuptools
+      argv = sys.argv[1:]                 # then fetch the arguments from the system
+
+  # qm_df = traf.load_tsv('test/resources/group_bold.tsv')
+  qm_df = traf.load_tsv('test/resources/gtest.tsv')
+  norm_df = traf.normalize_to_zscores(qm_df)
+  styler = traf.colorize_by_std_deviations(norm_df)
+  traf.write_table_to_html(styler, "/tmp/table.html")
+
+  fig, axes = plt.subplots(2, 1)
+  pos_ax, neg_ax = axes
+  traf.make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
+  traf.make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
+  plt.show()
+
+
+
+if __name__ == "__main__":
+  main()
