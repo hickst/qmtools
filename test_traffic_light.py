39ba83cd8c0338302c03d60b24e626e7f70ce623
Tom Hicks <hickst@tohono.com>
2021-07-17 19:33:17 -0700

    Updated for rename to traffic module.

--- test_traffic_light.py
+++ test_traffic_light.py
@@ -1,17 +1,17 @@
 from matplotlib import cm
 from matplotlib import pyplot as plt
 
-import qmview.qmview as qmv
-from qmview.qmview import TURNIP8_COLORMAP, TURNIP8_COLORMAP_R
+import qmview.traffic_light as traf
+from qmview.traffic_light import TURNIP8_COLORMAP, TURNIP8_COLORMAP_R
 
-# qm_df = qmv.load_tsv('test/resources/group_bold.tsv')
-qm_df = qmv.load_tsv('test/resources/gtest.tsv')
-norm_df = qmv.normalize_to_zscores(qm_df)
-styler = qmv.colorize_by_std_deviations(norm_df)
-qmv.write_table_to_html(styler, "/tmp/table.html")
+# qm_df = traf.load_tsv('test/resources/group_bold.tsv')
+qm_df = traf.load_tsv('test/resources/gtest.tsv')
+norm_df = traf.normalize_to_zscores(qm_df)
+styler = traf.colorize_by_std_deviations(norm_df)
+traf.write_table_to_html(styler, "/tmp/table.html")
 
 # fig, axes = plt.subplots(2, 1)
 # pos_ax, neg_ax = axes
-# qmv.make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
-# qmv.make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
+# traf.make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
+# traf.make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
 # plt.show()
