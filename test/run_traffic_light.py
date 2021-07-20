79090d981739f77ab4c7269cfb6345c1e5671c2c
Tom Hicks <hickst@tohono.com>
2021-07-20 15:21:48 -0700

    Cleanup run_traffic_light dev file to run everything so far.

--- test/run_traffic_light.py
+++ test/run_traffic_light.py
@@ -21,17 +21,11 @@ def main (argv=None):
 
   traf.make_legends()
 
-  # qm_df = traf.load_tsv('test/resources/group_bold.tsv')
-  # qm_df = traf.load_tsv('inputs/gtest.tsv')
-  # norm_df = traf.normalize_to_zscores(qm_df)
-  # styler = traf.colorize_by_std_deviations(norm_df)
-  # traf.write_table_to_html(styler, f"{REPORTS_DIR}/table.html")
-
-  # fig, axes = plt.subplots(2, 1)
-  # pos_ax, neg_ax = axes
-  # traf.make_legend_on_axis(pos_ax, TURNIP8_COLORMAP)
-  # traf.make_legend_on_axis(neg_ax, TURNIP8_COLORMAP_R)
-  # plt.show()
+  #  qm_df = traf.load_tsv('test/resources/group_bold.tsv')
+  qm_df = traf.load_tsv('inputs/gtest.tsv')
+  norm_df = traf.normalize_to_zscores(qm_df)
+  styler = traf.colorize_by_std_deviations(norm_df)
+  traf.write_table_to_html(styler, f"{REPORTS_DIR}/table.html")
 
 
 
