d9a0551c8cad0793e1b3a7407c1cedcc0823a5ae
Tom Hicks <hickst@tohono.com>
2021-07-22 19:25:02 -0700

    Add tests for pos_neg_split. Add/use modality test files & constants.

--- tests/qmview/test_traffic_light.py
+++ tests/qmview/test_traffic_light.py
@@ -1,6 +1,6 @@
 # Tests of the traffic-light table code.
 #   Written by: Tom Hicks and Dianne Patterson. 7/19/2021.
-#   Last Modified: Add/use test constants. Added test for write_table_to_html.
+#   Last Modified: Add tests for pos_neg_split. Add/use modality test files & constants.
 #
 import os
 import matplotlib
@@ -16,11 +16,23 @@ from tests import TEST_RESOURCES_DIR
 
 class TestTrafficLight(object):
 
-  gtest_tstfyl  = f"{TEST_RESOURCES_DIR}/gtest.tsv"
+  bold_test_fyl  = f"{TEST_RESOURCES_DIR}/bold_test.tsv"
+  struct_test_fyl  = f"{TEST_RESOURCES_DIR}/struct_test.tsv"
+
   html_min_size = 100000        # magic number for HTML report file (# bytes)
   legend_min_size = 6000        # magic number for legend (# bytes) in a .png file
   df_cell_count = 855           # size of test zscore dataframe
   df_shape = (19, 45)           # shape of test zscore dataframe
+
+  bold_pos_good_df_cell_count = 19 * len(traf.BOLD_POS_GOOD_COLUMNS)
+  bold_pos_good_df_shape = (19, len(traf.BOLD_POS_GOOD_COLUMNS))
+  bold_pos_bad_df_cell_count = 19 * len(traf.BOLD_POS_BAD_COLUMNS)
+  bold_pos_bad_df_shape = (19, len(traf.BOLD_POS_BAD_COLUMNS))
+
+  struct_pos_good_df_cell_count = 19 * len(traf.STRUCTURAL_POS_GOOD_COLUMNS)
+  struct_pos_good_df_shape = (19, len(traf.STRUCTURAL_POS_GOOD_COLUMNS))
+  struct_pos_bad_df_cell_count = 19 * len(traf.STRUCTURAL_POS_BAD_COLUMNS)
+  struct_pos_bad_df_shape = (19, len(traf.STRUCTURAL_POS_BAD_COLUMNS))
 
 
   def test_colorize_by_std_deviations(self):
@@ -39,7 +51,7 @@ class TestTrafficLight(object):
 
 
   def test_load_tsv(self):
-    qm_df = traf.load_tsv(self.gtest_tstfyl)
+    qm_df = traf.load_tsv(self.bold_test_fyl)
     print(qm_df)
     assert qm_df is not None
     assert type(qm_df) == pandas.core.frame.DataFrame
@@ -75,7 +87,7 @@ class TestTrafficLight(object):
 
 
   def test_normalize_to_zscores(self):
-    qm_df = traf.load_tsv(self.gtest_tstfyl)
+    qm_df = traf.load_tsv(self.bold_test_fyl)
     norm_df = traf.normalize_to_zscores(qm_df)
     assert type(norm_df) == pandas.core.frame.DataFrame
     assert norm_df.size == self.df_cell_count
@@ -86,10 +98,40 @@ class TestTrafficLight(object):
     assert type(norm_df.iloc[0, 1]) == numpy.float64
 
 
+  def test_pos_neg_split_bold(self):
+    qm_df = traf.load_tsv(self.bold_test_fyl)
+    (pos_good_df, pos_bad_df) = traf.pos_neg_split(qm_df, 'bold')
+
+    assert pos_good_df is not None
+    assert type(pos_good_df) == pandas.core.frame.DataFrame
+    assert pos_good_df.size == self.bold_pos_good_df_cell_count
+    assert pos_good_df.shape == self.bold_pos_good_df_shape
+
+    assert pos_bad_df is not None
+    assert type(pos_bad_df) == pandas.core.frame.DataFrame
+    assert pos_bad_df.size == self.bold_pos_bad_df_cell_count
+    assert pos_bad_df.shape == self.bold_pos_bad_df_shape
+
+
+  def test_pos_neg_split_struct(self):
+    qm_df = traf.load_tsv(self.struct_test_fyl)
+    (pos_good_df, pos_bad_df) = traf.pos_neg_split(qm_df, 'T1w')
+
+    assert pos_good_df is not None
+    assert type(pos_good_df) == pandas.core.frame.DataFrame
+    assert pos_good_df.size == self.struct_pos_good_df_cell_count
+    assert pos_good_df.shape == self.struct_pos_good_df_shape
+
+    assert pos_bad_df is not None
+    assert type(pos_bad_df) == pandas.core.frame.DataFrame
+    assert pos_bad_df.size == self.struct_pos_bad_df_cell_count
+    assert pos_bad_df.shape == self.struct_pos_bad_df_shape
+
+
   def test_write_table_to_html(self):
     with tempfile.TemporaryDirectory() as tmpdir:
       print(f"tmpdir={tmpdir}")
-      qm_df = traf.load_tsv(self.gtest_tstfyl)
+      qm_df = traf.load_tsv(self.bold_test_fyl)
       norm_df = traf.normalize_to_zscores(qm_df)
       styler = traf.colorize_by_std_deviations(norm_df)
       traf.write_table_to_html(styler, "table.html", dirpath=tmpdir)
