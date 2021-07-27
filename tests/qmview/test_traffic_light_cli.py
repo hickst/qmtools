eb7ba377b794c3a6ebcc809962163d89d3b2927a
Tom Hicks <hickst@tohono.com>
2021-07-27 16:09:55 -0700

    Add missing argument tests for main method.

--- tests/qmview/test_traffic_light_cli.py
+++ tests/qmview/test_traffic_light_cli.py
@@ -1,18 +1,20 @@
 # Tests of the traffic-light CLI code.
 #   Written by: Tom Hicks and Dianne Patterson. 7/27/2021.
-#   Last Modified: Initial creation.
+#   Last Modified: Add missing argument tests for main method.
 #
 import os
 import matplotlib
 import numpy
 import pandas
 import pytest
+import sys
 import tempfile
 from pathlib import Path
 
 import qmview.traffic_light_cli as cli
-from config.settings import REPORTS_DIR
 from tests import TEST_RESOURCES_DIR
+
+SYSEXIT_ERROR_CODE = 2                 # seems to be error exit code from argparse
 
 class TestTrafficLightCLI(object):
 
@@ -25,15 +27,13 @@ class TestTrafficLightCLI(object):
   def test_check_input_file_noarg(self):
     with pytest.raises(SystemExit) as se:
       cli.check_input_file(None)
-    assert se.type == SystemExit
-    assert se.value.code != 0          # expecting error exit code
+    assert se.value.code == cli.INPUT_FILE_EXIT_CODE
 
 
   def test_check_input_file_nosuchfile(self):
     with pytest.raises(SystemExit) as se:
       cli.check_input_file(self.nosuch_test_fyl)
-    assert se.type == SystemExit
-    assert se.value.code != 0          # expecting error exit code
+    assert se.value.code == cli.INPUT_FILE_EXIT_CODE
 
 
   def test_check_input_file_empty(self):
@@ -48,3 +48,42 @@ class TestTrafficLightCLI(object):
       cli.check_input_file(self.bold_test_fyl)
     except SystemExit as se:
       assert False, "check_input_file unexpectedly exited when given valid bold test file"
+
+
+  def test_main_noargs(self, capsys):
+    with pytest.raises(SystemExit) as se:
+      cli.main()
+    assert se.value.code == SYSEXIT_ERROR_CODE
+    sysout, syserr = capsys.readouterr()
+    print(f"CAPTURED SYS.ERR:\n{syserr}")
+    assert f"usage: {cli.PROG_NAME}" in syserr
+
+
+  def test_main_help(self, capsys):
+    with pytest.raises(SystemExit) as se:
+      sys.argv = ['qmview', '-h']
+      cli.main()
+    assert se.value.code == 0          # help is not an error for parseargs
+    sysout, syserr = capsys.readouterr()
+    print(f"CAPTURED SYS.OUT:\n{sysout}")
+    assert f"usage: {cli.PROG_NAME}" in sysout
+
+
+  def test_main_no_inputfile(self, capsys):
+    with pytest.raises(SystemExit) as se:
+      sys.argv = ['qmview', '-m', 'bold']
+      cli.main()
+    assert se.value.code == SYSEXIT_ERROR_CODE
+    sysout, syserr = capsys.readouterr()
+    print(f"CAPTURED SYS.ERR:\n{syserr}")
+    assert 'the following arguments are required: -i/--input-file' in syserr
+
+
+  def test_main_no_modality(self, capsys):
+    with pytest.raises(SystemExit) as se:
+      sys.argv = ['qmview', '-i', self.bold_test_fyl]
+      cli.main()
+    assert se.value.code == SYSEXIT_ERROR_CODE
+    sysout, syserr = capsys.readouterr()
+    print(f"CAPTURED SYS.ERR:\n{syserr}")
+    assert 'the following arguments are required: -m/--modality' in syserr
