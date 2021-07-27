77e298fb5ff58b61ded1f3afc06b0ffa2fdf4175
Tom Hicks <hickst@tohono.com>
2021-07-26 17:58:56 -0700

    Update setup.py for creation of traffic light CLI program.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='QMView',
-    version='0.0.2',
+    version='0.0.8',
     packages=find_packages(),
     # package_data={'qmview': ['resources/*.txt', 'resources/*.properties']},
     # include_package_data=True,
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'run_traffic  = tests.run_traffic_light:main'
+            'run_traffic  = qmview.traffic_light_cli:main'
         ]
     },
 )
