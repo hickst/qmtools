c0de3e7105bc51d624ea3c4dbaf9a33e40a993cd
Tom Hicks <hickst@tohono.com>
2021-08-03 17:05:03 -0700

    Bump version #. Add run_fetcher script.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='QMView',
-    version='0.0.8',
+    version='0.0.11',
     packages=find_packages(),
     # package_data={'qmview': ['resources/*.txt', 'resources/*.properties']},
     # include_package_data=True,
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'run_traffic  = qmview.traffic_light_cli:main'
+            'run_traffic = qmview.traffic_light_cli:main',
+            'run_fetcher = qmfetcher.fetcher_cli:main'
         ]
     },
 )
