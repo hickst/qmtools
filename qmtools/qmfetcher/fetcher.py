ee500ca1512fae5dfb95cd16b51029a968273c39
Tom Hicks <hickst@tohono.com>
2021-08-08 15:35:40 -0700

    Implemented clean_records, added tests for same.

--- qmtools/qmfetcher/fetcher.py
+++ qmtools/qmfetcher/fetcher.py
@@ -1,6 +1,6 @@
 # Author: Tom Hicks and Dianne Patterson.
 # Purpose: Methods to query the MRIQC server and download query result records.
-# Last Modified: Flesh out and test extract_records.
+# Last Modified: Implemented clean_records. Update some function doc strings.
 
 import os
 import pandas as pd
@@ -11,9 +11,14 @@ from qmtools.qmfetcher import DEFAULT_RESULTS_SIZE, SERVER_PAGE_SIZE
 from qmtools.qm_utils import validate_modality
 
 SERVER_URL = "https://mriqc.nimh.nih.gov/api/v1"
+FIELDS_TO_REMOVE = ['_etag', '_links']
 
 
 def build_query (modality, page_num=None, query_params=None):
+  """
+  Construct and return a query string given the modality, starting page number,
+  and optional dictionary of query parameter keys and values.
+  """
   validate_modality(modality)          # validates or raises ValueError
   url_str = f"{SERVER_URL}/{modality}?max_results={SERVER_PAGE_SIZE}"
   if (page_num is not None or query_params is not None):
@@ -26,7 +31,15 @@ def build_query (modality, page_num=None, query_params=None):
 
 
 def clean_records (json_recs):
-  # TODO: IMPLEMENT LATER
+  """
+  Remove unwanted fields from each record (dictionary) in the given list and
+  returned the list of cleaned records.
+  Arguments:
+    json_recs: a list of records, each one representing metrics for a single image.
+  """
+  for rec in json_recs:
+    for field in FIELDS_TO_REMOVE:
+      rec.pop(field)
   return json_recs
 
 
@@ -37,6 +50,10 @@ def do_query (query_str):
 
 
 def extract_records (json_query_result):
+  """
+  Extract and return a list of records (dictionaries) from the given
+  query result dictionary.
+  """
   return json_query_result['_items']
 
 
@@ -53,5 +70,4 @@ def query_for_page (modality, page_num=1, query_params=None):
   query = build_query(modality, page_num, query_params)
   json_query_result = do_query(query)
   json_recs = extract_records(json_query_result)
-  cleaned = clean_records(json_recs)
-  return cleaned
+  return clean_records(json_recs)
--- tests/qmtools/qmfetcher/test_fetcher.py
+++ tests/qmtools/qmfetcher/test_fetcher.py
@@ -1,6 +1,6 @@
 # Tests of the MRIQC data fetcher library code.
 #   Written by: Tom Hicks and Dianne Patterson. 8/7/2021.
-#   Last Modified: Test extract records.
+#   Last Modified: Add tests for clean_records.
 #
 import json
 import os
@@ -13,7 +13,7 @@ from pathlib import Path
 from qmtools import ALLOWED_MODALITIES
 from qmtools.qmfetcher import DEFAULT_RESULTS_SIZE, SERVER_PAGE_SIZE
 import qmtools.qmfetcher.fetcher as fetch
-from qmtools.qmfetcher.fetcher import SERVER_URL
+from qmtools.qmfetcher.fetcher import SERVER_URL, FIELDS_TO_REMOVE
 from tests import TEST_RESOURCES_DIR
 
 SYSEXIT_ERROR_CODE = 2                 # seems to be error exit code from argparse
@@ -49,10 +49,32 @@ class TestFetcher(object):
     assert qstr == f"{SERVER_URL}/bold?max_results={SERVER_PAGE_SIZE}&page=999"
 
 
+  def test_clean_records_empty(self):
+    with open(self.empty_results_fyl) as jfyl:
+      results = json.load(jfyl)
+    print(results)
+    recs = fetch.extract_records(results)
+    crecs = fetch.clean_records(recs)
+    assert crecs is not None
+    assert crecs == []
+
+
+  def test_clean_records_page1(self):
+    with open(self.page1_results_fyl) as jfyl:
+      results = json.load(jfyl)
+    recs = fetch.extract_records(results)
+    crecs = fetch.clean_records(recs)
+    assert crecs is not None
+    assert crecs != []
+    assert len(crecs) == self.page1_results_cnt
+    for rec in crecs:
+      for field in FIELDS_TO_REMOVE:
+        assert rec.get(field) is None
+
+
   def test_extract_records_empty(self):
     with open(self.empty_results_fyl) as jfyl:
       results = json.load(jfyl)
-    print(type(results))
     print(results)
     recs = fetch.extract_records(results)
     assert recs is not None
@@ -62,7 +84,6 @@ class TestFetcher(object):
   def test_extract_records_page1(self):
     with open(self.page1_results_fyl) as jfyl:
       results = json.load(jfyl)
-    print(type(results))
     print(results)
     recs = fetch.extract_records(results)
     assert recs is not None
