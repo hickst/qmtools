a1238d7812514a811c1213486b6f16129083a146
Tom Hicks <hickst@tohono.com>
2021-08-10 19:56:00 -0700

    Add gen_output_filename and ensure_fetched_dir. Add tests for same.

--- qmtools/qm_utils.py
+++ qmtools/qm_utils.py
@@ -1,10 +1,41 @@
 # Author: Tom Hicks and Dianne Patterson.
 # Purpose: Shared utilities for the QMTools programs.
-# Last Modified: Initial creation by refactoring.
+# Last Modified: Add gen_output_filename and ensure_fetched_dir.
 #
+import datetime
+import os
 import pandas as pd
+import sys
 
-from qmtools import ALLOWED_MODALITIES
+from qmtools import ALLOWED_MODALITIES, FETCHED_DIR, FETCHED_DIR_EXIT_CODE
+from qmtools.file_utils import good_dir_path
+
+
+def ensure_fetched_dir (program_name):
+  """
+  Check that the given output directory path is a valid path. If not, then exit
+  the entire program here with the specified (or default) system exit code.
+  """
+  if (FETCHED_DIR is None or (not good_dir_path(FETCHED_DIR, writeable=True))):
+    try:
+      os.mkdir(FETCHED_DIR, mode=0o775)
+    except OSError:
+      helpMsg =  """
+        There must be a writeable child subdirectory called 'fetched'
+        to hold the fetched query results.
+        """
+      errMsg = "({}): ERROR: {} Exiting...".format(program_name, helpMsg)
+      print(errMsg, file=sys.stderr)
+      sys.exit(FETCHED_DIR_EXIT_CODE)
+
+
+def gen_output_filename (modality, extension='tsv'):
+  """
+  Generate and return a default name based on modality, timestamp, and optional extension.
+  """
+  time_now = datetime.datetime.now()
+  now_str = time_now.strftime("%Y%m%d_%H%M%S-%f")
+  return f"{modality}_{now_str}.{extension}"
 
 
 def load_tsv (tsv_path):
--- tests/qmtools/test_qm_utils.py
+++ tests/qmtools/test_qm_utils.py
@@ -1,13 +1,15 @@
 # Tests of Shared utilities for the QMTools programs.
 #   Written by: Tom Hicks and Dianne Patterson. 8/5/2021.
-#   Last Modified: Split shared function tests to here.
+#   Last Modified: Added tests for gen_output_filename, ensure_fetched_dir.
 #
 import os
 import pandas
 import pytest
+import tempfile
 
 from tests import TEST_RESOURCES_DIR
-from qmtools.qm_utils import load_tsv, validate_modality
+from qmtools import FETCHED_DIR, FETCHED_DIR_EXIT_CODE
+import qmtools.qm_utils as qmu
 
 
 class TestQMUtils(object):
@@ -17,8 +19,49 @@ class TestQMUtils(object):
   df_shape = (19, 45)           # shape of test dataframe
 
 
+  def test_ensure_fetched_dir(self):
+    with tempfile.TemporaryDirectory() as tmpdir:
+      os.chdir(tmpdir)
+      assert not os.path.isdir(FETCHED_DIR)
+      qmu.ensure_fetched_dir('TestQMUtils')
+      assert os.path.isdir(FETCHED_DIR)
+      assert os.access(FETCHED_DIR, os.W_OK)
+
+
+  def test_ensure_fetched_dir_fail(self):
+    if (os.environ.get('RUNNING_IN_CONTAINER') is None):
+      with pytest.raises(SystemExit) as se:
+        os.chdir('/')
+        assert not os.path.isdir(FETCHED_DIR)
+        qmu.ensure_fetched_dir('TestQMUtils')
+      assert se.value.code == FETCHED_DIR_EXIT_CODE
+    else:
+      assert True
+
+
+  def test_gen_output_filename(self):
+    fname = qmu.gen_output_filename('bold')
+    print(fname)
+    assert 'bold' in fname
+    assert '.tsv' in fname
+
+
+  def test_gen_output_filename_badmode(self):
+    fname = qmu.gen_output_filename('BOLDER')
+    print(fname)
+    assert 'BOLDER' in fname
+    assert '.tsv' in fname
+
+
+  def test_gen_output_filename_ext(self):
+    fname = qmu.gen_output_filename('t1w', 'csv')
+    print(fname)
+    assert 't1w' in fname
+    assert '.csv' in fname
+
+
   def test_load_tsv(self):
-    qm_df = load_tsv(self.bold_test_fyl)
+    qm_df = qmu.load_tsv(self.bold_test_fyl)
     print(qm_df)
     assert qm_df is not None
     assert type(qm_df) == pandas.core.frame.DataFrame
@@ -27,27 +70,27 @@ class TestQMUtils(object):
 
 
   def test_validate_modality_good(self):
-    assert validate_modality('bold') == 'bold'
-    assert validate_modality('t1w') == 't1w'
-    assert validate_modality('t2w') == 't2w'
-    assert validate_modality('Bold') == 'bold'
-    assert validate_modality('T1w') == 't1w'
-    assert validate_modality('T2w') == 't2w'
-    assert validate_modality('BOLD') == 'bold'
-    assert validate_modality('T1W') == 't1w'
-    assert validate_modality('T2W') == 't2w'
-    assert validate_modality('BoLd') == 'bold'
+    assert qmu.validate_modality('bold') == 'bold'
+    assert qmu.validate_modality('t1w') == 't1w'
+    assert qmu.validate_modality('t2w') == 't2w'
+    assert qmu.validate_modality('Bold') == 'bold'
+    assert qmu.validate_modality('T1w') == 't1w'
+    assert qmu.validate_modality('T2w') == 't2w'
+    assert qmu.validate_modality('BOLD') == 'bold'
+    assert qmu.validate_modality('T1W') == 't1w'
+    assert qmu.validate_modality('T2W') == 't2w'
+    assert qmu.validate_modality('BoLd') == 'bold'
 
 
   def test_validate_modality_fail(self):
     with pytest.raises(ValueError, match='Modality argument must be one of'):
-      validate_modality('')
+      qmu.validate_modality('')
 
     with pytest.raises(ValueError, match='Modality argument must be one of'):
-      validate_modality('BAD argument')
+      qmu.validate_modality('BAD argument')
 
     with pytest.raises(ValueError, match='Modality argument must be one of'):
-      validate_modality('T1')
+      qmu.validate_modality('T1')
 
     with pytest.raises(ValueError, match='Modality argument must be one of'):
-      validate_modality('t1')
+      qmu.validate_modality('t1')
