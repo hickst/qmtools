26eb3706bb1a3a5f80e0ac52d29d13fef22d8d74
Tom Hicks <hickst@tohono.com>
2021-08-16 20:24:22 -0700

    Fix tests failing by interference (shame, shame).

--- tests/qmtools/qmfetcher/test_fetcher.py
+++ tests/qmtools/qmfetcher/test_fetcher.py
@@ -1,6 +1,6 @@
 # Tests of the MRIQC data fetcher library code.
 #   Written by: Tom Hicks and Dianne Patterson. 8/7/2021.
-#   Last Modified: Added test for save_to_tsv method.
+#   Last Modified: Fix tests failing by interference (shame, shame).
 #
 import json
 import os
@@ -19,16 +19,9 @@ from tests import TEST_RESOURCES_DIR
 
 SYSEXIT_ERROR_CODE = 2                 # seems to be error exit code from argparse
 
-class TestFetcher(object):
-
-  noresult_query = 'https://mriqc.nimh.nih.gov/api/v1/bold?max_records=1&where=bids_meta.Manufacturer%3D%3D"BADCO"'
-
-  empty_results_fyl = f"{TEST_RESOURCES_DIR}/no_results.json"
-  page1_results_fyl = f"{TEST_RESOURCES_DIR}/reptime1.json"
-  page1_results_cnt = 25
-
-  arec_len = 23
-  arec = {
+@pytest.fixture
+def arec():
+  return {
     '_id': '59cfad7f265d200019380537',
     'fber': 3165.384765625,
     'dummy_trs': 0,
@@ -64,29 +57,10 @@ class TestFetcher(object):
     }
   }
 
-  flrec = {
-    '_id': '59cfad7f265d200019380537',
-    'fber': 3165.384765625,
-    'dummy_trs': 0,
-    'bids_meta.RepetitionTime': 2.0,
-    'bids_meta.modality': 'bold',
-    'bids_meta.Manufacturer': 'Siemens',
-    'bids_meta.EchoTime': 0.026,
-    'gcor': 0.00348879,
-    'fwhm_x': 2.266005,
-    '_created': 'Sat, 30 Sep 2017 14:43:11 GMT',
-    'provenance.md5sum': 'af34ecc2a49a550d49216f5b5b5b22d7',
-    'provenance.settings.fd_thres': 0.2,
-    'provenance.settings.hmc_fsl': False,
-    'snr': 5.489635677064641,
-    '_updated': 'Sat, 30 Sep 2017 14:43:11 GMT',
-    '_etag': 'c93ade3cea8db90a9d1f6f1d4433effe5ce7192c',
-    'spacing_z': 1.9999998807907104,
-    '_links.self.title': 'bold',
-    '_links.self.href': 'bold/59cfad7f265d200019380537'
-  }
 
-  for_dedup_recs = [
+@pytest.fixture
+def dedup_recs():
+  return [
     {
       '_id': '1',
       'provenance.md5sum': '19cf39e8895fcf98e46f6017caebbbf1',
@@ -108,6 +82,41 @@ class TestFetcher(object):
     }
   ]
 
+
+@pytest.fixture
+def flrec():
+  return {
+    '_id': '59cfad7f265d200019380537',
+    'fber': 3165.384765625,
+    'dummy_trs': 0,
+    'bids_meta.RepetitionTime': 2.0,
+    'bids_meta.modality': 'bold',
+    'bids_meta.Manufacturer': 'Siemens',
+    'bids_meta.EchoTime': 0.026,
+    'gcor': 0.00348879,
+    'fwhm_x': 2.266005,
+    '_created': 'Sat, 30 Sep 2017 14:43:11 GMT',
+    'provenance.md5sum': 'af34ecc2a49a550d49216f5b5b5b22d7',
+    'provenance.settings.fd_thres': 0.2,
+    'provenance.settings.hmc_fsl': False,
+    'snr': 5.489635677064641,
+    '_updated': 'Sat, 30 Sep 2017 14:43:11 GMT',
+    '_etag': 'c93ade3cea8db90a9d1f6f1d4433effe5ce7192c',
+    'spacing_z': 1.9999998807907104,
+    '_links.self.title': 'bold',
+    '_links.self.href': 'bold/59cfad7f265d200019380537'
+}
+
+
+class TestFetcher(object):
+
+  noresult_query = 'https://mriqc.nimh.nih.gov/api/v1/bold?max_records=1&where=bids_meta.Manufacturer%3D%3D"BADCO"'
+
+  empty_results_fyl = f"{TEST_RESOURCES_DIR}/no_results.json"
+  page1_results_fyl = f"{TEST_RESOURCES_DIR}/reptime1.json"
+  page1_results_cnt = 25
+
+  arec_len = 23
 
   def test_build_query_bad_modality(self):
     with pytest.raises(ValueError) as ve:
@@ -154,8 +163,8 @@ class TestFetcher(object):
     assert crecs == []
 
 
-  def test_clean_records(self):
-    crecs = fetch.clean_records([self.flrec])
+  def test_clean_records(self, flrec):
+    crecs = fetch.clean_records([flrec])
     assert crecs is not None
     assert crecs != []
     assert len(crecs) == 1
@@ -166,50 +175,78 @@ class TestFetcher(object):
         assert rec.get(field) is None
 
 
-  def test_deduplicate_records(self):
+  def test_deduplicate_records_0_1st(self, dedup_recs):
     # Also tests is_not_duplicate
-    recs = fetch.deduplicate_records(self.for_dedup_recs,set())
+    recs, _ = fetch.deduplicate_records(dedup_recs, set())
+    print(recs)
     assert recs is not None
     assert len(recs) == 3
 
-    recs = fetch.deduplicate_records(self.for_dedup_recs,
+
+  def test_deduplicate_records_0_2nd(self, dedup_recs):
+    # Also tests is_not_duplicate
+    recs, _ = fetch.deduplicate_records(dedup_recs,
       set(['8f56e0168c25e7bd919572669c2e43be'])) # not in set: has no effect
+    print(recs)
     assert recs is not None
     assert len(recs) == 3
 
-    recs = fetch.deduplicate_records(self.for_dedup_recs,
+
+  def test_deduplicate_records_1_1st(self, dedup_recs):
+    # Also tests is_not_duplicate
+    recs, _ = fetch.deduplicate_records(dedup_recs,
       set(['42febfb2ff72767655b0901dbde42ecb']))      # should remove 1 record
+    print(recs)
     assert recs is not None
     assert len(recs) == 2
 
-    recs = fetch.deduplicate_records(self.for_dedup_recs,
+
+  def test_deduplicate_records_1_2nd(self, dedup_recs):
+    # Also tests is_not_duplicate
+    recs, _ = fetch.deduplicate_records(dedup_recs,
       set(['42febfb2ff72767655b0901dbde42ecb',       # should remove record w/ this key
        '8f56e0168c25e7bd919572669c2e43be']))
+    print(recs)
     assert recs is not None
     assert len(recs) == 2
 
-    recs = fetch.deduplicate_records(self.for_dedup_recs,
+
+  def test_deduplicate_records_2_1st(self, dedup_recs):
+    # Also tests is_not_duplicate
+    recs, _ = fetch.deduplicate_records(dedup_recs,
       set(['42febfb2ff72767655b0901dbde42ecb',       # should remove this record
        '19cf39e8895fcf98e46f6017caebbbf1']))      # should remove this record
+    print(recs)
     assert recs is not None
     assert len(recs) == 1
 
-    recs = fetch.deduplicate_records(self.for_dedup_recs,
+
+  def test_deduplicate_records_2_2nd(self, dedup_recs):
+    # Also tests is_not_duplicate
+    recs, _ = fetch.deduplicate_records(dedup_recs,
       set([ '69b0e488119a656f70d881e6112a41cd',
         '42febfb2ff72767655b0901dbde42ecb',       # should remove this record
         '19cf39e8895fcf98e46f6017caebbbf1',       # should remove this record
         'c54e9578f22a0efebe32128291e1c947']))
+    print(recs)
     assert recs is not None
     assert len(recs) == 1
 
-    recs = fetch.deduplicate_records(self.for_dedup_recs,
+
+  def test_deduplicate_records_3_1st(self, dedup_recs):
+    # Also tests is_not_duplicate
+    recs, _ = fetch.deduplicate_records(dedup_recs,
       set([ '42febfb2ff72767655b0901dbde42ecb',       # should remove these records
         '19cf39e8895fcf98e46f6017caebbbf1',
         'af34ecc2a49a550d49216f5b5b5b22d7']))
+    print(recs)
     assert recs is not None
     assert len(recs) == 0
 
-    recs = fetch.deduplicate_records(self.for_dedup_recs,
+
+  def test_deduplicate_records_3_2nd(self, dedup_recs):
+    # Also tests is_not_duplicate
+    recs, _ = fetch.deduplicate_records(dedup_recs,
       set([ 'cb948823526737aa29f11e3acd1c8dd2',
         '42febfb2ff72767655b0901dbde42ecb',       # should remove this record
         '5805b03d4ea8cfd577bff761e321f0b9',
@@ -217,6 +254,7 @@ class TestFetcher(object):
         '19cf39e8895fcf98e46f6017caebbbf1',       # should remove this record
         'af34ecc2a49a550d49216f5b5b5b22d7',       # should remove this record
         'cb948823526737aa29f11e3acd1c8dd2']))
+    print(recs)
     assert recs is not None
     assert len(recs) == 0
 
@@ -229,9 +267,12 @@ class TestFetcher(object):
 
 
   def test_do_query_noresults(self):
-    recs = fetch.do_query(self.noresult_query)
-    assert recs is not None
-    assert recs == []
+    rec = fetch.do_query(self.noresult_query)
+    assert rec is not None
+    assert type(rec) == dict
+    assert '_items' in rec
+    assert '_meta' in rec
+    assert (rec.get('_meta')).get('total') == 0
 
 
   def test_extract_records_empty(self):
@@ -253,8 +294,8 @@ class TestFetcher(object):
     assert len(recs) == self.page1_results_cnt
 
 
-  def test_flatten_a_record(self):
-    tups = fetch.flatten_a_record(self.arec)
+  def test_flatten_a_record(self, arec):
+    tups = fetch.flatten_a_record(arec)
     print(f"TUPS[{len(tups)}]={tups}")
     assert tups is not None
     assert len(tups) == self.arec_len
@@ -275,8 +316,8 @@ class TestFetcher(object):
     assert 'aqi' not in d
 
 
-  def test_flatten_records(self):
-    flist = fetch.flatten_records([self.arec, self.arec])
+  def test_flatten_records(self, arec):
+    flist = fetch.flatten_records([arec, arec])
     assert flist is not None
     assert len(flist) == 2
     assert type(flist[0]) == dict
@@ -364,16 +405,18 @@ class TestFetcher(object):
     assert len(recs) == SERVER_PAGE_SIZE
 
 
-  def test_save_to_tsv_1(self):
+  def test_save_to_tsv_1(self, flrec):
     with tempfile.TemporaryDirectory() as tmpdir:
       print(f"tmpdir={tmpdir}")
       tmpfile = os.path.join(tmpdir, 'test.tsv')
-      fetch.save_to_tsv([self.flrec], tmpfile)
+      fetch.save_to_tsv([flrec], tmpfile)
       files = os.listdir(os.path.join(tmpdir))
       assert files is not None
       assert len(files) == 1
       with open(tmpfile) as tmpf:
         lines = tmpf.readlines()
+        print(f"LINES[0]={lines[0]}")
+        print(f"LINES[1]={lines[1]}")
       assert len(lines) == 2
       assert '_id' in lines[0]
       assert '_created' in lines[0]
