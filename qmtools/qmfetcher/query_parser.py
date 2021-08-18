abbd5d776d074c35dbb0da0035961ee867a81a85
Tom Hicks <hickst@tohono.com>
2021-08-17 19:05:04 -0700

    Refactor load from parse. Update for rename to load_query_from_file. Add nocolon test for same.

--- qmtools/qmfetcher/query_parser.py
+++ qmtools/qmfetcher/query_parser.py
@@ -1,31 +1,51 @@
 #
 # Module with methods to read and parse a query parameters file.
 #   Written by: Tom Hicks. 8/17/2021.
-#   Last Modified: Initial creation
+#   Last Modified: Refactor load from parse.
 #
 import configparser as cp
+from configparser import MissingSectionHeaderError, ParsingError
+import sys
 
 
 def parse_query_from_file (query_file, prog_name=''):
     """
-    Load and parse a set of query parameters from the given query parameters file.
+    Load and validate a set of query parameters from the given query parameters file.
+    Return a dictionary of valid query parameters or raise exceptions.
     """
+    qparams = load_query_from_file(query_file, prog_name)
+    # TODO: validate_query_params(qparams)
+    return qparams
+
+
+def load_query_from_file (query_file, prog_name=''):
+    """
+    Load query parameters from the 'parameters' section of the given
+    query parameters file. Return those parameters as a dictionary
+    or raise exceptions for various loading problems.
+    """
+    sys.tracebacklimit = 0
     try:
       config = cp.ConfigParser(strict=False, empty_lines_in_values=False)
       config.optionxform = lambda option: option
       config.read_file(open(query_file))
-    except FileNotFoundError:
+    except FileNotFoundError as fnf:
       errMsg = "({}): ERROR: {} Exiting...".format(prog_name,
         "Query parameters file '{query_file}' not found or not readable.")
       raise FileNotFoundError(errMsg)
-    except cp.MissingSectionHeaderError:
-      errMsg = "A 'parameters' section header must be included in the query parameters file '{query_file}'."
+    except MissingSectionHeaderError as mse:
+      errMsg = "({}): ERROR: {} Exiting...".format(prog_name,
+        "A 'parameters' section header must be included in the query parameters file.")
+      raise ValueError(errMsg)
+    except ParsingError as pe:
+      errMsg = "({}): ERROR: {} Exiting...".format(prog_name, pe)
       raise ValueError(errMsg)
 
     try:
       qparams = config['parameters']
-    except KeyError:
-      errMsg = "No section named 'parameters' found in query parameters file '{query_file}'."
+    except KeyError as ke:
+      errMsg = "({}): ERROR: {} Exiting...".format(prog_name,
+        "No section named 'parameters' found in query parameters file.")
       raise ValueError(errMsg)
 
     return dict(qparams)
--- tests/qmtools/qmfetcher/test_query_parser.py
+++ tests/qmtools/qmfetcher/test_query_parser.py
@@ -1,6 +1,6 @@
 # Tests of the module to read and parse a query parameters file.
 #   Written by: Tom Hicks and Dianne Patterson. 8/17/2021.
-# Last Modified: Initial createion.
+# Last Modified: Update for rename to load_query_from_file. Add nocolon test for same.
 #
 import configparser
 import os
@@ -15,33 +15,42 @@ SYSEXIT_ERROR_CODE = 2                 # seems to be error exit code from argpar
 
 class TestQueryParser(object):
 
+  TEST_NAME = 'TestQueryParser'
+
   empty_query_fyl = f"{TEST_RESOURCES_DIR}/empty.qp"
-  nosec_query_fyl = f"{TEST_RESOURCES_DIR}/nosec.qp"
+  nocolon_query_fyl = f"{TEST_RESOURCES_DIR}/nocolon.qp"
   noparams_query_fyl = f"{TEST_RESOURCES_DIR}/noparams.qp"
+  nosec_query_fyl = f"{TEST_RESOURCES_DIR}/nosec.qp"
   params_query_fyl = f"{TEST_RESOURCES_DIR}/manmaf.qp"
 
-  def test_parse_query_from_file_nosuch(self):
+  def test_load_query_from_file_nosuch(self):
     with pytest.raises(FileNotFoundError) as fnf:
-      qp.parse_query_from_file('NOSUCH_FILE')
+      qp.load_query_from_file('NOSUCH_FILE', self.TEST_NAME)
 
 
-  def test_parse_query_from_file_empty(self):
+  def test_load_query_from_file_empty(self):
     with pytest.raises(ValueError) as ve:
-      qp.parse_query_from_file(self.empty_query_fyl)
+      qp.load_query_from_file(self.empty_query_fyl, self.TEST_NAME)
 
 
-  def test_parse_query_from_file_nosec(self):
+  def test_load_query_from_file_nosec(self):
     with pytest.raises(ValueError) as ve:
-      qp.parse_query_from_file(self.nosec_query_fyl)
+      qp.load_query_from_file(self.nosec_query_fyl, self.TEST_NAME)
 
 
-  def test_parse_query_from_file_noparams(self):
+  def test_load_query_from_file_noparams(self):
     with pytest.raises(ValueError) as ve:
-      qp.parse_query_from_file(self.noparams_query_fyl)
+      qp.load_query_from_file(self.noparams_query_fyl, self.TEST_NAME)
 
 
-  def test_parse_query_from_file_params(self):
-    pd = qp.parse_query_from_file(self.params_query_fyl)
+  def test_load_query_from_file_nocolon(self):
+    with pytest.raises(ValueError) as ve:
+      pd = qp.load_query_from_file(self.nocolon_query_fyl, self.TEST_NAME)
+    print(ve)
+
+
+  def test_load_query_from_file_params(self):
+    pd = qp.load_query_from_file(self.params_query_fyl, self.TEST_NAME)
     assert pd is not None
     assert type(pd) is dict
     assert len(pd) == 3
