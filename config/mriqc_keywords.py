82957e5fbfe70c9102b5ee98b778f6e6a06c18b3
Tom Hicks <hickst@tohono.com>
2021-08-19 20:11:39 -0700

    Initial creation of data structure stubs.

---
+++ config/mriqc_keywords.py
@@ -0,0 +1,16 @@
+#
+# Module containing sets of MRIQC schema keywords; to be used for validation.
+#   Written by: Tom Hicks. 8/19/2021.
+#   Last Modified: Initial creation of data structure stubs.
+#
+BIDS_KEYWORDS = set(
+
+)
+
+BOLD_KEYWORDS = set(
+
+)
+
+STRUCTURAL_KEYWORDS = set(
+
+)
