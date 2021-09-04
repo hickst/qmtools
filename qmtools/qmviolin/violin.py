dd21d43473afded1c49974e097494eadddfe206a
Tom Hicks <hickst@tohono.com>
2021-09-03 20:04:08 -0700

    Add method to clean dataframes before merging.

--- qmtools/qmviolin/violin.py
+++ qmtools/qmviolin/violin.py
@@ -1,12 +1,11 @@
 # Methods to create IMQ violin plots from two MRIQC datasets.
 #   Written by: Tom Hicks and Dianne Patterson. 9/3/2021.
-#   Last Modified: Initial creation of stub.
+#   Last Modified: Add method to clean dataframes before merging.
 #
 import csv
-# import json
 import os
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import sys
 
 from config.mriqc_keywords import STRUCT_HI_GOOD_COLUMNS, STRUCT_LO_GOOD_COLUMNS
@@ -14,6 +13,10 @@ from config.mriqc_keywords import BOLD_HI_GOOD_COLUMNS, BOLD_LO_GOOD_COLUMNS
 from qmtools import ALLOWED_MODALITIES, STRUCTURAL_MODALITIES
 from qmtools.qmfetcher import DEFAULT_RESULTS_SIZE, SERVER_PAGE_SIZE
 import qmtools.qm_utils as qmu
+from qmtools.qmview.traffic_light import write_table_to_tsv   # REMOVE LATER
+
+# Tuple of column name prefix strings which should be removed from fetched data files:
+COLUMNS_TO_REMOVE = ('_', 'bids_meta', 'provenance', 'rating')
 
 
 def vplot (modality, args):
@@ -33,9 +36,27 @@ def vplot (modality, args):
     raise FileNotFoundError("Required 'group_file' filepath not found in arguments dictionary")
 
   fetch_df = qmu.load_tsv(fetched_file)
+  clean_df(fetch_df)
   group_df = qmu.load_tsv(group_file)
+  clean_df(group_df)
 
-  print('FETCHED DataFrame:\n')
-  print(fetch_df)
-  print('GROUP DataFrame:\n')
-  print(group_df)
+  merge_df = pd.concat([fetch_df.assign(orig='fetch'), group_df.assign(orig='group')])
+  print('MERGED DataFrame:\n')           # REMOVE LATER
+  print(merge_df)                        # REMOVE LATER
+  rpt_name = args.get('report_filename', 'mergy')
+  write_table_to_tsv(merge_df, rpt_name)  # REMOVE LATER
+
+
+def clean_df (df):
+  """
+  Renames ID fields to merge them and removes unneeded columns, by side effect!
+  Assumes that applying this function to a group file has no effect (because the
+  columns to alter don't exist).
+  """
+  # Rename the fetched records unique ID (_id) so that it merges with the
+  # unique ID (bids_name) field of the group file.
+  df.rename(columns={'_id': 'bids_name'}, errors='ignore', inplace=True)
+
+  # Remove extra fields from the fetch data records
+  del_list = [col for col in list(df.columns) if col.startswith(COLUMNS_TO_REMOVE)]
+  df.drop(columns=del_list, errors='ignore', inplace=True)
