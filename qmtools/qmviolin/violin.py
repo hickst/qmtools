8b2f2ca47811e15382fcee19d42cc576ae9f9bf1
Tom Hicks <hickst@tohono.com>
2021-09-08 14:55:58 -0700

    WIP: Produce all violin plot files.

--- qmtools/qmviolin/violin.py
+++ qmtools/qmviolin/violin.py
@@ -1,6 +1,6 @@
 # Methods to create IMQ violin plots from two MRIQC datasets.
 #   Written by: Tom Hicks and Dianne Patterson. 9/3/2021.
-#   Last Modified: Continue developing: select IQMs and begin do_plots.
+#   Last Modified: Produce all violin plot files.
 #
 # import csv
 # import os
@@ -13,7 +13,8 @@ import seaborn as sb
 from config.mriqc_keywords import (BOLD_HI_GOOD_COLUMNS, BOLD_LO_GOOD_COLUMNS,
                                    STRUCT_HI_GOOD_COLUMNS, STRUCT_LO_GOOD_COLUMNS)
 import qmtools.qm_utils as qmu
-from qmtools import ALLOWED_MODALITIES, STRUCTURAL_MODALITIES
+from qmtools.qmview.traffic_light import write_figure_to_file  # REMOVE LATER
+from qmtools import PLOT_EXT, STRUCTURAL_MODALITIES
 from qmtools.qmview.traffic_light import write_table_to_tsv  # REMOVE LATER
 
 # Tuple of column name prefix strings which should be removed from fetched data files:
@@ -45,14 +46,12 @@ def vplot (modality, args):
   merged_df = pd.concat([fetch_df.assign(source='fetch'), group_df.assign(source='group')])
 
   # pivot the merged dataframe to index by bids_name and source
-  melted_df = merged_df.melt(id_vars=['bids_name', 'source'])
+  melted_df = merged_df.melt(id_vars=['bids_name', 'source'], var_name='IQM')
 
-  print('\nMELTED DataFrame:\n')           # REMOVE LATER
-  print(melted_df)                        # REMOVE LATER
-  rpt_name = args.get('report_filename', 'melty')
-  write_table_to_tsv(melted_df, rpt_name)  # REMOVE LATER
+  print('\nMELTED DataFrame:\n')       # REMOVE LATER
+  print(melted_df)                     # REMOVE LATER
 
-  do_plots(modality, melted_df)
+  do_plots(modality, args, melted_df)
 
 
 def clean_df (df):
@@ -70,16 +69,36 @@ def clean_df (df):
   df.drop(columns=del_list, errors='ignore', inplace=True)
 
 
-def do_plots (modality, plot_df, plot_iqms=None):
+def do_plots (modality, args, iqms_df, plot_iqms=None):
   """
   Takes a merged and melted MRIQC dataset and an optional list of IQMs to plot
   and plots the given or default IQMs.
   """
   iqms_to_plot = select_iqms_to_plot(modality, plot_iqms)
   print(f"IQMs={iqms_to_plot}")        # REMOVE LATER
-  # for iqm in iqms_to_plot:
-  #   TODO: IMPLEMENT LATER
-  #   do_a_plot(modality, plot_df, iqm)
+  for iqm in iqms_to_plot:
+    do_a_plot(modality, args, iqms_df, iqm)
+
+
+def do_a_plot (modality, args, iqms_df, iqm):
+  """
+  Select the data for the specified IQM from the given IQMs dataset and plot it.
+  """
+  plot_df = iqms_df[iqms_df['IQM'] == iqm]
+  vplot = sb.catplot(x="IQM", y="value", hue="source", data=plot_df,
+                     kind="violin", inner="quartile", split=True, palette="pastel")
+  filename = gen_plot_filename(modality, iqm)
+  write_figure_to_file(vplot, filename)
+  # TODO: gen_html_for_iqm(modality, args, iqm, filename)
+
+
+def gen_plot_filename (modality, iqm_name, extension=PLOT_EXT):
+  """
+  Generate and return a default name based on modality, IQM name, and optional extension.
+  """
+  # time_now = datetime.datetime.now()
+  # now_str = time_now.strftime("%Y%m%d_%H%M%S-%f")
+  return f"{modality}_{iqm_name}{extension}"
 
 
 def select_iqms_to_plot (modality, plot_iqms=None):
