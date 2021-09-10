68536604702babaf21c64b3aca8085e1d8491e28
Tom Hicks <hickst@tohono.com>
2021-09-10 13:41:52 -0700

    Add tests for gen_plot_filename.

--- tests/qmtools/qmviolin/test_violin.py
+++ tests/qmtools/qmviolin/test_violin.py
@@ -1,12 +1,39 @@
 # Tests of the IMQ Violin plotting modules.
 #   Written by: Tom Hicks and Dianne Patterson. 9/7/2021.
-#   Last Modified: Initial creation: test_select_iqms_to_plot only.
+#   Last Modified: Add tests for gen_plot_filename.
 #
 import qmtools.qm_utils as qmu
 import qmtools.qmviolin.violin as violin
+from qmtools import PLOT_EXT
 
 
 class TestViolin(object):
+
+  def test_gen_plot_filename(self):
+    fname = violin.gen_plot_filename('bold', 'aor')
+    print(fname)
+    assert 'bold' in fname
+    assert 'aor' in fname
+    assert PLOT_EXT in fname
+
+
+  def test_gen_plot_filename_ext(self):
+    fname = violin.gen_plot_filename('T1w', 'cjv', extension='.plot')
+    print(fname)
+    assert 'T1w' in fname
+    assert 'cjv' in fname
+    assert '.plot' in fname
+    assert PLOT_EXT not in fname
+
+
+  def test_gen_plot_filename_fake(self):
+    "NB: this test will succeed since no semantic checking is done."
+    fname = violin.gen_plot_filename('BOLDER', 'NOSUCH', extension='.crazy')
+    print(fname)
+    assert 'BOLDER' in fname
+    assert 'NOSUCH' in fname
+    assert '.crazy' in fname
+
 
   def test_select_iqms_to_plot_bold(self):
     iqms = violin.select_iqms_to_plot('bold')
